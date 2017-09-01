f6a9ceac2923b0ba3f9419404a55c8e1ed535f71
mattgoldman93 <mattgoldman93@gmail.com>
2017-09-01 09:33:32 -0400

    reset button
    adds functioning reset button to tic-tac-toe game

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -12,30 +12,39 @@ const checkForWin = function () {
   if (i < 10) {
     if (($('#box1').text() === $('#box2').text() && $('#box2').text() === $('#box3').text()) && ($('#box1').text() === 'X' || $('#box1').text() === 'O')) {
       $('#display').text(player + ' wins!')
+      jQuery('#resetBoard').addClass('btn-success').removeClass('btn-danger')
       i = 10
     } else if (($('#box1').text() === $('#box5').text() && $('#box5').text() === $('#box9').text()) && ($('#box1').text() === 'X' || $('#box1').text() === 'O')) {
       $('#display').text(player + ' wins!')
+      jQuery('#resetBoard').addClass('btn-success').removeClass('btn-danger')
       i = 10
     } else if (($('#box1').text() === $('#box4').text() && $('#box4').text() === $('#box7').text()) && ($('#box1').text() === 'X' || $('#box1').text() === 'O')) {
       $('#display').text(player + ' wins!')
+      jQuery('#resetBoard').addClass('btn-success').removeClass('btn-danger')
       i = 10
     } else if (($('#box2').text() === $('#box5').text() && $('#box5').text() === $('#box8').text()) && ($('#box2').text() === 'X' || $('#box2').text() === 'O')) {
       $('#display').text(player + ' wins!')
+      jQuery('#resetBoard').addClass('btn-success').removeClass('btn-danger')
       i = 10
     } else if (($('#box3').text() === $('#box6').text() && $('#box6').text() === $('#box9').text()) && ($('#box3').text() === 'X' || $('#box3').text() === 'O')) {
       $('#display').text(player + ' wins!')
+      jQuery('#resetBoard').addClass('btn-success').removeClass('btn-danger')
       i = 10
     } else if (($('#box4').text() === $('#box5').text() && $('#box5').text() === $('#box6').text()) && ($('#box4').text() === 'X' || $('#box4').text() === 'O')) {
       $('#display').text(player + ' wins!')
+      jQuery('#resetBoard').addClass('btn-success').removeClass('btn-danger')
       i = 10
     } else if (($('#box3').text() === $('#box5').text() && $('#box5').text() === $('#box7').text()) && ($('#box3').text() === 'X' || $('#box3').text() === 'O')) {
       $('#display').text(player + ' wins!')
+      jQuery('#resetBoard').addClass('btn-success').removeClass('btn-danger')
       i = 10
     } else if (($('#box7').text() === $('#box8').text() && $('#box8').text() === $('#box9').text()) && ($('#box7').text() === 'X' || $('#box7').text() === 'O')) {
       $('#display').text(player + ' wins!')
+      jQuery('#resetBoard').addClass('btn-success').removeClass('btn-danger')
       i = 10
     } else if (i === 9) {
       $('#display').text('no winner this time 😢')
+      jQuery('#resetBoard').addClass('btn-success').removeClass('btn-danger')
     }
   }
 }
@@ -50,36 +59,12 @@ $(() => {
     i++
     checkForWin()
   })
+  $('#resetBoard').on('click', function () {
+    event.preventDefault()
+    $('.box').text(null)
+    $('.box').attr('disabled', false)
+    $('#display').text(null)
+    jQuery('#resetBoard').addClass('btn-danger')
+    i = 0
+  })
 })
-
-// find a way to record which is where
-// 'X's will all be even and O will all be odd
-// find way to pull value as content and store in new variable that can be compared
-// for click function
-
-// $(() => {
-//   setAPIOrigin(location, config)
-// })
-
-// use require with a reference to bundle the file and use it in this file
-// const example = require('./example')
-
-// use require without a reference to ensure a file is bundled
-// require('./example')
-
-/*
-Once turn is over Value of player is set to = to
-the following will be code that will check which player is passed in
- function (player) {
-    turn = null
-    if (player = player_x){
-    turn = 'X'
-  } else {
-  turn = 'O'
-  //now variable turn will be passed into new function tentatively called post()
-  post(turn)
-}
-}
-
-let turn = x
-*/
